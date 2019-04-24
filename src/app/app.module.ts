7620ca75aa2d182db902be02be0a68abaae64e14
apomic80 <michele.aponte@blexin.com>
2019-04-24 06:16:49 +0200

    start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { SampleListComponent } from './sample-list/sample-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SampleListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sample-list/sample-list.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample-list',
+  templateUrl: './sample-list.component.html'
+})
+export class SampleListComponent {
+
+  text: string;
+  items: {label: string, value: number}[] = [];
+
+  addTextToList() {
+    const randomValue =  Math.floor(Math.random() * 10) + 1;
+    this.items.push({ label: this.text, value: randomValue });
+  }
+
+  generateValue(value: number) {
+    console.log('value ' + value + ' generated');
+    return this.fibonacci(value * 4);
+  }
+
+  private fibonacci(num: number): number {
+    if (num === 1 || num === 2) {
+      return 1;
+    }
+    return this.fibonacci(num - 1) + this.fibonacci(num - 2);
+  }
+}
